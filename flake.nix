9aed927bb845d963f99d7dfaae73267a342c96e6
Robin Stumm <robin.stumm@iohk.io>
2023-06-27 12:14:06 +0200

    add .#hydraJobs

--- flake.nix
+++ flake.nix
@@ -149,150 +149,154 @@
         };
       };
 
-      flake.nixosModules.auth-keys-hub = {
-        config,
-        pkgs,
-        lib,
-        ...
-      }: let
-        cfg = config.programs.auth-keys-hub;
-      in {
-        options = {
-          programs.auth-keys-hub = {
-            enable = lib.mkEnableOption "auth-keys-hub";
+      flake = { config, ... }: {
+        hydraJobs = builtins.mapAttrs (_: v: removeAttrs v ["default"]) config.packages;
 
-            package = lib.mkOption {
-              type = lib.types.package;
-              default = pkgs.auth-keys-hub;
-              description = "The derivation to use for /bin/auth-keys-hub";
-            };
-
-            user = lib.mkOption {
-              type = lib.types.str;
-              default = "auth-keys-hub";
-              description = "The user executing the AuthorizedKeysCommand";
-            };
-
-            group = lib.mkOption {
-              type = lib.types.str;
-              default = "auth-keys-hub";
-              description = "The group of the user executing the AuthorizedKeysCommand";
-            };
-
-            dataDir = lib.mkOption {
-              type = lib.types.str;
-              default = "/run/auth-keys-hub";
-              description = "Directory used to cache the authorized_keys file";
-            };
-
-            ttl = lib.mkOption {
-              type = lib.types.str;
-              default = "1h";
-              description = "how often to fetch new keys, format is 1d2h3m4s";
-            };
-
-            gitlab = {
-              host = lib.mkOption {
-                type = lib.types.str;
-                default = "gitlab.com";
-                description = "Change this when using self hosted GitLab";
-              };
-
-              users = lib.mkOption {
-                type = lib.types.listOf lib.types.str;
-                default = [];
-                description = "List of GitLab user names that are allowed to login";
-              };
-
-              groups = lib.mkOption {
-                type = lib.types.listOf lib.types.str;
-                default = [];
-                description = "List of GitLab groups that are allowed to login";
-              };
-
-              tokenFile = lib.mkOption {
-                type = lib.types.nullOr lib.types.str;
-                default = null;
-                description = "Read the GitLab token from this file, required when groups are used";
-              };
-            };
-
-            github = {
-              host = lib.mkOption {
-                type = lib.types.str;
-                default = "github.com";
-                description = "Change this when using GitHub Enterprise";
-              };
-
-              users = lib.mkOption {
-                type = lib.types.listOf lib.types.str;
-                default = [];
-                description = "List of GitHub user names that are allowed to login";
-              };
-
-              teams = lib.mkOption {
-                type = lib.types.listOf lib.types.str;
-                default = [];
-                description = "list of org/team that are allowed access (e.g. acme/ops)";
-              };
-
-              tokenFile = lib.mkOption {
-                type = lib.types.nullOr lib.types.str;
-                default = null;
-                description = "Read the GitHub token from this file, required when org/team is used";
-              };
-            };
-          };
-        };
-
-        config = lib.mkIf cfg.enable (let
-          join = builtins.concatStringsSep ",";
-
-          flags = lib.cli.toGNUCommandLine {} {
-            inherit (cfg) ttl;
-            dir = cfg.dataDir;
-
-            github-host = cfg.github.host;
-            github-users = join cfg.github.users;
-            github-teams = join cfg.github.teams;
-            github-token-file = cfg.github.tokenFile;
-
-            gitlab-host = cfg.gitlab.host;
-            gitlab-users = join cfg.gitlab.users;
-            gitlab-groups = join cfg.gitlab.groups;
-            gitlab-token-file = cfg.gitlab.tokenFile;
-          };
+        nixosModules.auth-keys-hub = {
+          config,
+          pkgs,
+          lib,
+          ...
+        }: let
+          cfg = config.programs.auth-keys-hub;
         in {
-          assertions = [
-            {
-              assertion = cfg.github.teams == [] || cfg.github.tokenFile != null;
-              message = "programs.auth-keys-hub.github.teams requires tokenFile to be set as well";
-            }
-          ];
+          options = {
+            programs.auth-keys-hub = {
+              enable = lib.mkEnableOption "auth-keys-hub";
 
-          users.users.${cfg.user} = {
-            description = "auth-keys-hub user";
-            isSystemUser = true;
-            inherit (cfg) group;
+              package = lib.mkOption {
+                type = lib.types.package;
+                default = pkgs.auth-keys-hub;
+                description = "The derivation to use for /bin/auth-keys-hub";
+              };
+
+              user = lib.mkOption {
+                type = lib.types.str;
+                default = "auth-keys-hub";
+                description = "The user executing the AuthorizedKeysCommand";
+              };
+
+              group = lib.mkOption {
+                type = lib.types.str;
+                default = "auth-keys-hub";
+                description = "The group of the user executing the AuthorizedKeysCommand";
+              };
+
+              dataDir = lib.mkOption {
+                type = lib.types.str;
+                default = "/run/auth-keys-hub";
+                description = "Directory used to cache the authorized_keys file";
+              };
+
+              ttl = lib.mkOption {
+                type = lib.types.str;
+                default = "1h";
+                description = "how often to fetch new keys, format is 1d2h3m4s";
+              };
+
+              gitlab = {
+                host = lib.mkOption {
+                  type = lib.types.str;
+                  default = "gitlab.com";
+                  description = "Change this when using self hosted GitLab";
+                };
+
+                users = lib.mkOption {
+                  type = lib.types.listOf lib.types.str;
+                  default = [];
+                  description = "List of GitLab user names that are allowed to login";
+                };
+
+                groups = lib.mkOption {
+                  type = lib.types.listOf lib.types.str;
+                  default = [];
+                  description = "List of GitLab groups that are allowed to login";
+                };
+
+                tokenFile = lib.mkOption {
+                  type = lib.types.nullOr lib.types.str;
+                  default = null;
+                  description = "Read the GitLab token from this file, required when groups are used";
+                };
+              };
+
+              github = {
+                host = lib.mkOption {
+                  type = lib.types.str;
+                  default = "github.com";
+                  description = "Change this when using GitHub Enterprise";
+                };
+
+                users = lib.mkOption {
+                  type = lib.types.listOf lib.types.str;
+                  default = [];
+                  description = "List of GitHub user names that are allowed to login";
+                };
+
+                teams = lib.mkOption {
+                  type = lib.types.listOf lib.types.str;
+                  default = [];
+                  description = "list of org/team that are allowed access (e.g. acme/ops)";
+                };
+
+                tokenFile = lib.mkOption {
+                  type = lib.types.nullOr lib.types.str;
+                  default = null;
+                  description = "Read the GitHub token from this file, required when org/team is used";
+                };
+              };
+            };
           };
 
-          users.groups.${cfg.group} = {};
+          config = lib.mkIf cfg.enable (let
+            join = builtins.concatStringsSep ",";
 
-          systemd.tmpfiles.rules = ["d ${cfg.dataDir} 0700 ${cfg.user} ${cfg.group} - -"];
+            flags = lib.cli.toGNUCommandLine {} {
+              inherit (cfg) ttl;
+              dir = cfg.dataDir;
 
-          environment.etc."ssh/auth-keys-hub" = {
-            mode = "0755";
-            text = ''
-              #!${pkgs.bash}/bin/bash
-              exec ${cfg.package}/bin/auth-keys-hub ${lib.escapeShellArgs flags}
-            '';
-          };
+              github-host = cfg.github.host;
+              github-users = join cfg.github.users;
+              github-teams = join cfg.github.teams;
+              github-token-file = cfg.github.tokenFile;
 
-          services.openssh = {
-            authorizedKeysCommand = "/etc/ssh/auth-keys-hub";
-            authorizedKeysCommandUser = cfg.user;
-          };
-        });
+              gitlab-host = cfg.gitlab.host;
+              gitlab-users = join cfg.gitlab.users;
+              gitlab-groups = join cfg.gitlab.groups;
+              gitlab-token-file = cfg.gitlab.tokenFile;
+            };
+          in {
+            assertions = [
+              {
+                assertion = cfg.github.teams == [] || cfg.github.tokenFile != null;
+                message = "programs.auth-keys-hub.github.teams requires tokenFile to be set as well";
+              }
+            ];
+
+            users.users.${cfg.user} = {
+              description = "auth-keys-hub user";
+              isSystemUser = true;
+              inherit (cfg) group;
+            };
+
+            users.groups.${cfg.group} = {};
+
+            systemd.tmpfiles.rules = ["d ${cfg.dataDir} 0700 ${cfg.user} ${cfg.group} - -"];
+
+            environment.etc."ssh/auth-keys-hub" = {
+              mode = "0755";
+              text = ''
+                #!${pkgs.bash}/bin/bash
+                exec ${cfg.package}/bin/auth-keys-hub ${lib.escapeShellArgs flags}
+              '';
+            };
+
+            services.openssh = {
+              authorizedKeysCommand = "/etc/ssh/auth-keys-hub";
+              authorizedKeysCommandUser = cfg.user;
+            };
+          });
+        };
       };
     };
 }
