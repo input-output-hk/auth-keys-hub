eb80a503d6b2eaf1193473677ee7f8def60dd03d
John Lotoski <john.lotoski@iohk.io>
2023-12-20 16:37:13 -0600

    system: add aarch64-linux package

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
-      systems = ["x86_64-linux" "aarch64-darwin"];
+      systems = ["x86_64-linux" "aarch64-darwin" "aarch64-linux"];
 
       imports = [
         inputs.treefmt-nix.flakeModule
